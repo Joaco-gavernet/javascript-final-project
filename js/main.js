e56e445a762207cfbcba4eb9d000423828ca30ea
Joaquín Gavernet <joaquingavernet@gmail.com>
2021-01-23 20:06:37 -0300

    Nueva funcion productBuilder() que se encarga de mostrar en el DOM los elementos en el objeto JSON 'productData.js'. Deje una version hecha atravez de los nodos del DOM comentada y otra version que implementa inner.HTML='' para no tener que escribir tanto codigo y para que sea mas dinamica. Ademas al mostrar esta funcion con un forEach() le sume un condicional que oculta los elementos que no estan activos, es decir, cuya propiedad 'isActive' es 'false'

--- js/main.js
+++ js/main.js
@@ -45,27 +45,13 @@
 
 
 
-
-// Muestro los productos
-console.log('Productos:');
-productData.forEach(function (i) {
-  // If para chequear que el producto este disponible y lo imprimo en la consola
-  if ((i.stock > 0) && (i.isActive)) {
-    return new Product(i).getProductData();
-  }
-})
-
-
-
-
-
 // INTENTO - FUNCION PARA INSTANCIAR AUTOMATICAMENTE LOS OBJETOS DE PRODUCTDATA.JS ------------------------------------
 
 
 // // Intento de definir una variable tomando el "id" de cada instancia del objeto para llamarlo con ese nombre. Esto es por que en el caso anterior en el que instancio al objeto Product.js y lo imprimo en la consola con su metodo, no puedo volver a llamar a cada una de las instancias por su nombre
 // function productInstancer (i, x) {
-//   window[x] = new Product(i);
-//   return window[x];
+//   let product = new Product(i);
+//   return product;
 // }
 // console.log(productInstancer(productData, productData.id));
 
@@ -80,7 +66,9 @@ productData.forEach(function (i) {
 
 
 
-// // Intento hacer un loop para definir las instancias y los nombres de las variables instanciadas automaticamente.
+// INTENTO - LOOP PARA DEFINIR LAS INSTANCIAS Y LOS NOMBRES AUTOMATICAMENTE --------------------------------------
+
+
 // // Defino la variable que indica la longitud del JSON "productData" para pasarla como detonante en el parametro del "for"
 // let size = productData.length;
 // for (let i = 0; i < size; i++){
@@ -89,12 +77,24 @@ productData.forEach(function (i) {
 // }
 
 
+// FIN DE INTENTO ------------------------------------
+
+
 
 
 
 // // Defino las instancias de los objetos manualmente porque no pude plantear una funcion que les asigne un nombre acorde a la cantidad de productos instanciados en base al JSON "productData.js". 
 // let product1 = new Product(productData[0]);
 // let product2 = new Product(productData[1]);
+
+// // Muestro los productos
+// console.log('Productos:');
+// productData.forEach(function (i) {
+//   // corroboro disponibilidad y actividad
+//   if ((i.stock > 0) && (i.isActive)) {
+//     return new Product(i).getProductData();
+//   }
+// })
 
 // // Ejecucion del carrito de compras
 // let carrito = new Cart();
@@ -109,3 +109,81 @@ productData.forEach(function (i) {
 
 // // Ejemplo de recargo de stock
 // product2.changeStock(+15);
+
+
+
+
+
+// INTENTO - FUNCION PARA CONTRUIR LOS PRODUCTOS ------------------------------------
+
+
+/* CONSIGNAS:
+  - Construir el contenedor en HTML donde van a estar ubicados los productos del
+  - Guardar en una variable el contenedor
+  - 
+*/
+
+
+function productBuilder(product) {
+  // let liProducto = document.createElement('li');
+  // let contenedorProducto = document.createElement('div');
+  // let portada = document.createElement('img');
+  // let contenedorInfo = document.createElement('div');
+  // let title = document.createElement('h2');
+  // let author = document.createElement('h4');
+  // let editorial = document.createElement('h4');
+  // let description = document.createElement('p');
+  // let price = document.createElement('h3')
+  // let input = document.createElement('input');
+
+
+  // portada.src = product.img;
+  // title.textContent = product.title;
+  // author.textContent = product.author;
+  // editorial.textContent = product.editorial;
+  // description.textContent = product.description;
+  // price.textContent = product.price;
+  // input.type = "button";
+  // input.value = "Agregar";
+
+  // liProducto.appendChild(contenedorProducto);
+  // contenedorProducto.appendChild(portada);
+  // contenedorProducto.appendChild(contenedorInfo);
+  // contenedorInfo.appendChild(title);
+  // contenedorInfo.appendChild(author);
+  // contenedorInfo.appendChild(editorial);
+  // contenedorInfo.appendChild(description);
+  // contenedorInfo.appendChild(price);
+  // contenedorInfo.appendChild(input);
+
+  // return liProducto;
+
+  let result = `
+  <li class="productosCatalogo_ulLi">
+    <div>
+      <img src="${product.img}" alt="Portada de libro" class="productImg">
+      <div>
+        <h2 class="productTitle">${product.title}</h2>
+        <h4 class="productSubtitle">${product.author}</h4>
+        <h4 class="productSubtitle">${product.editorial}</h4>
+        <p class="productDescription">${product.description}</p>
+        <h3 class="productPrice">${product.price}</h3>
+        <input type="button" id="buttonComprar" value="Agregar" class="productButton">
+      </div>
+    </div>
+  </li>
+  `;
+  return result;
+}
+
+let contenedorCatalogo = document.querySelector('.productosCatalogo_ul');
+
+productData.forEach(function (product){
+  if (product.isActive) {
+    contenedorCatalogo.innerHTML += productBuilder(product);
+  }
+})
+
+
+
+// FIN DE INTENTO - FUNCION PARA CONTRUIR LOS PRODUCTOS -------------------------------------------
